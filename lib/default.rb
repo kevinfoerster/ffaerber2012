e851d5cc290fd75037fcc7ce07809bb137586b5f
Kevin Foerster <kevin.foerster@gmail.com>
2012-05-06 16:42:38 +0200

    removed some bullshitty files added some rules for media handling included some reps for content as image

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,30 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+require 'RMagick'
+require 'awesome_print'
+
+class ResizeAndCropFilter < Nanoc3::Filter
+  identifier :resizeAndCrop
+  type :binary
+
+  def run(filename, params={})  
+    img   = Magick::ImageList.new(filename)
+    thumb = img.resize_to_fill(params[:width], params[:height])
+    thumb.write output_filename
+  end
+end
+
+class GetNestedMedia < Nanoc3::Filter
+  identifier :getNestedMedia
+  type :text
+
+  def run(content, params={})
+    filter = params[:source]
+    media = @items.select {|it| 
+      it.identifier =~ %r{#{filter}} && it.binary? == true
+    }
+    
+    media.first.path(:rep => :thumbnail)
+  end
+end
